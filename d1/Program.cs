c419c1e3ce76e62a8826f0407fd2e7b160992522
Maximilian Neundorfer <code@mneundorfer.de>
2020-12-03 21:57:01 +0100

    aoc day 1

---
+++ d1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace d1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var raw = File.ReadLines("input.txt");
+            var entries = raw.Select(int.Parse).ToList();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                for (int j = i + 1; j < entries.Count - i; j++)
+                {
+                    var result = entries[i] + entries[j];
+                    if (result == 2020) {
+                        Console.WriteLine("Result Pt. 1:");
+                        Console.WriteLine($"{entries[i] * entries[j]}");
+                    }
+                }
+            }
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                for (int j = i + 1; j < entries.Count; j++)
+                {
+                    for (int k = j + 1; k < entries.Count ; k++)
+                    {
+                        var result = entries[i] + entries[j] + entries[k];
+                        if (result == 2020) {
+                            Console.WriteLine("Result Pt. 2:");
+                            Console.WriteLine($"{entries[i] * entries[j] * entries[k]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
